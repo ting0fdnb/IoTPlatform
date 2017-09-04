67465d44dd8af32d4c471001f9697faed1af0fcd
mc <983771837@qq.com>
2017-09-04 11:39:53 +0800

    update

--- src/main/java/edu/bupt/platform/IoT/controller/OperationController.java
+++ src/main/java/edu/bupt/platform/IoT/controller/OperationController.java
@@ -18,6 +18,7 @@ import edu.bupt.platform.IoT.service.OperationService;
  * @version V1.0.0
  * @createTime：2017年8月11日 下午6:13:50
  */
+
 @RequestMapping("/operate")
 @RestController
 public class OperationController extends BaseController {
@@ -46,15 +47,22 @@ public class OperationController extends BaseController {
 	@RequestMapping("/signUp")
 	@ResponseBody
 	public IoTResult signUp(HttpServletRequest request, @RequestParam(required = true) String deviceName,
-			@RequestParam(required = true) String signMessage) {
+			@RequestParam(required = true) String typeCode, @RequestParam(required = true) String manufacturer,
+			@RequestParam(required = true) String deviceCode, @RequestParam(required = false) String description,
+			@RequestParam(required = false) String picUrl, @RequestParam(required = false) String domain) {
 
+		String signMessage = getSignMessage(deviceName, typeCode, manufacturer, deviceCode, description, picUrl, domain);
+		
 		VerifyResult ver = operationService.verifySignUpMessage(signMessage);
 		
 		if(ver.getCode() == 0) {
 			String save = operationService.saveSignUpMessage(ver);
 			return new IoTResult(0, "", save);
 		}
-		
 		return new IoTResult(1, "verify failed", null);
+	}
+	
+	private static String getSignMessage(String deviceName, String typeCode, String manufacturer, String deviceCode, String description, String picUrl, String domain){
+		return deviceName + "&&" +  typeCode  + "&&" +  manufacturer  + "&&" +  deviceCode  + "&&" +  description  + "&&" +  picUrl  + "&&" +  domain;
 	}
 }
